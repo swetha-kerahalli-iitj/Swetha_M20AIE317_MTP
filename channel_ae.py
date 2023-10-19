95a16e1eb92b51e3f35f11db59ec27f1e5c5ceff
swetha-kerahalli-iitj <kerahalli.1@iitj.ac.in>
2023-10-19 23:59:29 +0530

    Revert " Latest changes"
    This reverts commit 8edf69761aee2147aa1ebeb036ffcbbfd3bc38c9.

--- channel_ae.py
+++ channel_ae.py
@@ -21,21 +21,21 @@ class Channel_AE(torch.nn.Module):
 
     def forward(self, input, fwd_noise,channeltype):
         # Setup Interleavers.
-        if self.args.is_interleave == 0:
-            pass
-
-        elif self.args.is_same_interleaver == 0:
-            interleaver = RandInterlv.RandInterlv(self.args.block_len, np.random.randint(0, 1000))
-
-            p_array = interleaver.p_array
-            self.enc.set_interleaver(p_array)
-            self.dec.set_interleaver(p_array)
-
-        else:  # self.args.is_same_interleaver == 1
-            interleaver = RandInterlv.RandInterlv(self.args.block_len, 0)  # not random anymore!
-            p_array = interleaver.p_array
-            self.enc.set_interleaver(p_array)
-            self.dec.set_interleaver(p_array)
+        # if self.args.is_interleave == 0:
+        #     pass
+        #
+        # elif self.args.is_same_interleaver == 0:
+        #     interleaver = RandInterlv.RandInterlv(self.args.block_len, np.random.randint(0, 1000))
+        #
+        #     p_array = interleaver.p_array
+        #     self.enc.set_interleaver(p_array)
+        #     self.dec.set_interleaver(p_array)
+        #
+        # else:  # self.args.is_same_interleaver == 1
+        #     interleaver = RandInterlv.RandInterlv(self.args.block_len, 0)  # not random anymore!
+        #     p_array = interleaver.p_array
+        #     self.enc.set_interleaver(p_array)
+        #     self.dec.set_interleaver(p_array)
 
         codes = self.enc(input)
 
@@ -74,62 +74,3 @@ class Channel_AE(torch.nn.Module):
         x_dec = self.dec(received_codes)
 
         return x_dec, codes
-
-class Channel_ModAE(torch.nn.Module):
-    def __init__(self, args, enc, dec, mod, demod, modulation = 'qpsk'):
-        super(Channel_ModAE, self).__init__()
-        use_cuda = not args.no_cuda and torch.cuda.is_available()
-        self.this_device = torch.device("cuda" if use_cuda else "cpu")
-
-        self.args = args
-        self.enc = enc
-        self.dec = dec
-        self.mod = mod
-        self.demod = demod
-
-    def forward(self, input, fwd_noise,channeltype,blocklen):
-        # Setup Interleavers.
-        if self.args.is_interleave == 0:
-            pass
-
-        elif self.args.is_same_interleaver == 0:
-            interleaver = RandInterlv.RandInterlv(blocklen, np.random.randint(0, 1000))
-
-            p_array = interleaver.p_array
-            self.enc.set_interleaver(p_array)
-            self.dec.set_interleaver(p_array)
-
-        else:# self.args.is_same_interleaver == 1
-            interleaver = RandInterlv.RandInterlv(blocklen, 0) # not random anymore!
-            p_array = interleaver.p_array
-            self.enc.set_interleaver(p_array)
-            self.dec.set_interleaver(p_array)
-
-        codes  = self.enc(input)
-        symbols = self.mod(codes)
-
-        # Setup channel mode:
-        if channeltype in ['awgn', 't-dist', 'radar', 'ge_awgn']:
-            received_symbols = symbols + fwd_noise
-
-        elif channeltype == 'fading':
-            data_shape = symbols.shape
-            #  Rayleigh Fading Channel, non-coherent
-            fading_h = torch.sqrt(torch.randn(data_shape) ** 2 + torch.randn(data_shape) ** 2) / torch.sqrt(
-                torch.tensor(3.14 / 2.0))  # np.sqrt(2.0)
-            fading_h = fading_h.type(torch.FloatTensor).to(self.this_device)
-            sample = fading_h * symbols
-            received_symbols = fading_h * symbols + fwd_noise
-
-        else:
-            print('default AWGN channel')
-            received_symbols = symbols + fwd_noise
-
-        if self.args.rec_quantize:
-            myquantize = MyQuantize.apply
-            received_symbols = myquantize(received_symbols, self.args.rec_quantize_level, self.args.rec_quantize_level)
-
-        x_rec = self.demod(received_symbols)
-        x_dec = self.dec(x_rec)
-
-        return x_dec, symbols
